c9c8e6094d98706468c54c01e8bab7baaab3b143
IfElseSwitch <dygit4lis@gmail.com>
2014-10-02 14:25:32 +0200

    Engine v0.1
    Mouvements implémentés : Se déplacer, Sauter.
    Mouvement non implémentés : Glisser, Saut mural, ?

---
+++ UnityProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour 
+{
+    public Vector2 speed = new Vector2(10, 20);
+    private Vector2 movement;
+    bool onGround = false;
+	
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
+        if (BS != null)
+        {
+            onGround = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
+        if (BS != null)
+        {
+            onGround = false;
+        }
+    }
+	// Update is called once per frame
+	void Update () 
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = 0;
+        bool jump = Input.GetButtonDown("Jump");
+        
+        if (jump && onGround)
+        {
+            inputY = 1;
+        }
+        
+        movement = new Vector2(speed.x * inputX, (speed.y * inputY ) + rigidbody2D.velocity.y);
+	}
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = movement;
+    }
+}
