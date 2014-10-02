52899a703ee9e1f957b1c6db3b78f0f7974dfbad
IfElseSwitch <dygit4lis@gmail.com>
2014-10-02 16:37:16 +0200

    Engine v0.2
    Ajout d'un scrolling avec 5 couches de parallaxe et deux sprites mappé
    en 3d.
    La caméra suis le joueur et se place derrière lui automatiquement.
    Les pièges peuvent tuer le joueur.

---
+++ UnityProject/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public float playerDirection = 1;
+    private double xMod = 1;
+    private double step = 0.1;
+    private float xBase = 7;
+    private float yBase = (float)1.5;
+    private float zBase = -15;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (xMod > (double)playerDirection) xMod -= step;
+        if (xMod < (double)playerDirection) xMod += step;
+        Vector3 localPos = new Vector3((float)(xMod * xBase), yBase, zBase);
+        this.transform.localPosition = localPos;
+	}
+}
--- UnityProject/Assets/Scripts/PlayerScript.cs
+++ UnityProject/Assets/Scripts/PlayerScript.cs
@@ -2,9 +2,11 @@
 
 public class PlayerScript : MonoBehaviour 
 {
-    public Vector2 speed = new Vector2(10, 20);
+    public Vector2 speed = new Vector2(15, 25);
+    public Vector3 CheckPoint = new Vector3(-14, 1, 0);
+    private int Direction = 0;
     private Vector2 movement;
-    bool onGround = false;
+    private bool onGround = false;
 	
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -12,6 +14,13 @@ public class PlayerScript : MonoBehaviour
         if (BS != null)
         {
             onGround = true;
+        }
+        TrapScript TS = collision.gameObject.GetComponent<TrapScript>();
+        if (TS != null)
+        {
+            Vector3 newPos = new Vector3(CheckPoint.x, CheckPoint.y, CheckPoint.z);
+            Direction = 0;
+            this.transform.localPosition = newPos;
         }
     }
     void OnCollisionExit2D(Collision2D collision)
@@ -25,7 +34,12 @@ public class PlayerScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-        float inputX = Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Horizontal") != 0) 
+        {
+            Direction = (int)(Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal")));
+        }
+        CameraScript cs = this.GetComponentInChildren<CameraScript>();
+        cs.playerDirection = Direction;
         float inputY = 0;
         bool jump = Input.GetButtonDown("Jump");
         
@@ -34,7 +48,7 @@ public class PlayerScript : MonoBehaviour
             inputY = 1;
         }
         
-        movement = new Vector2(speed.x * inputX, (speed.y * inputY ) + rigidbody2D.velocity.y);
+        movement = new Vector2(speed.x * Direction, (speed.y * inputY ) + rigidbody2D.velocity.y);
 	}
     void FixedUpdate()
     {
