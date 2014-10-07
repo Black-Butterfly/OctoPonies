df8817477924d74eced78135cdba737b1b23482e
armadon13127 <jerome.rousseau@etu.univ-amu.fr>
2014-10-08 00:41:38 +0200

    Détection collision v2
    Le personnage détecte maintenant correctement les contacts avec le bloc,
    il fait la distinction entre mur et sol.

--- UnityProject/Assets/Scripts/PlayerScript2.cs
+++ UnityProject/Assets/Scripts/PlayerScript2.cs
@@ -5,8 +5,8 @@ public class PlayerScript2 : MonoBehaviour
     public Vector2 speed = new Vector2(15, 25);
     public Vector3 CheckPoint = new Vector3(-14, 2, 0);
     public float deathY = -5;
-    private int Direction = 0;
-	private int lastDirection = 0;
+    public int Direction = 0;
+	public int lastDirection = 0;
     private Vector2 movement;
     public bool onGround = false;
 	public bool onWall = false;
@@ -17,46 +17,35 @@ public class PlayerScript2 : MonoBehaviour
         Direction = 0;
         this.transform.localPosition = newPos;
     }
-    
-    void OnTriggerEnter2D(Collider2D collision){
-        BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
-        if (BS != null)
-        {
-			lastDirection = Direction;
-            Direction = 0;
-			onWall = true;
-        }
-    }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
-        BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
-        if (BS != null)
-        {
-            onGround = true;
-        }
-        TrapScript TS = collision.gameObject.GetComponent<TrapScript>();
-        if (TS != null)
-        {
-            Death();
-        }
-    }
+		Vector2 hit = collision.contacts[0].normal;
+
+		float angle = Vector2.Angle(hit, Vector2.right);
+		
+		if (Mathf.Approximately(angle, 0)) { // Gauche
+			onWall = true;
+			Direction = 0;
+		}
+		if (Mathf.Approximately(angle, 180)) { // Droite
+			onWall = true;
+			Direction = 0;
+		}
+		if (Mathf.Approximately(angle, 90)) { // Bas
+			onGround = true;
+		}
+	}
     void OnCollisionExit2D(Collision2D collision)
     {
         BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
         if (BS != null)
         {
-            onGround = false;
+			if((rigidbody2D.velocity.x == 0 && rigidbody2D.velocity.y != 0 && !onGround) || rigidbody2D.velocity.x != 0)
+				onWall = false;
+			if(rigidbody2D.velocity.y != 0)
+				onGround = false;
         }
     }
-	void OnTriggerExit2D(Collider2D collision)
-	{
-		BlockScript BS = collision.gameObject.GetComponent<BlockScript>();
-		if (BS != null)
-		{
-			onWall = false;
-		}
-	}
 	// Update is called once per frame
 	void Update () 
     {
@@ -64,7 +53,10 @@ public class PlayerScript2 : MonoBehaviour
         {
 			int newDirection = (int)(Input.GetAxis("Horizontal") / Mathf.Abs(Input.GetAxis("Horizontal")));
 			if(!(onWall && lastDirection == newDirection))
+			{
+				lastDirection = Direction;
 				Direction = newDirection;
+			}
         }
         CameraScript cs = this.GetComponentInChildren<CameraScript>();
         //cs.playerDirection = Direction;
@@ -73,17 +65,18 @@ public class PlayerScript2 : MonoBehaviour
         
         if (jump && onGround)
         {
-            inputY = 1;
+			inputY = speed.y * 1;
         }
 		else if (jump && onWall && !onGround)
 		{
 			Direction = -(lastDirection);
-			inputY = 1.1f;
+			lastDirection = Direction;
+			inputY = (speed.y * 1) - rigidbody2D.velocity.y;
 		}
 
         if (this.transform.localPosition.y < deathY) Death();
 
-        movement = new Vector2(speed.x * Direction, (speed.y * inputY ) + rigidbody2D.velocity.y);
+        movement = new Vector2(speed.x * Direction, inputY + rigidbody2D.velocity.y);
 		rigidbody2D.velocity = movement;
 	}
     void FixedUpdate()
