9f3164609b75c44629d17c5dd76bc797283ae9b3
armadon13127 <jerome.rousseau@etu.univ-amu.fr>
2014-10-22 08:44:48 +0200

    MenuPause v1
    Met le jeu en pause, affiche un menu.
    BUG : Prend en compte les inputs pendant la pause
    LIMITS : Impossible de choisir le menu avec la manette

---
+++ UnityProject/Assets/Scripts/UpdateScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpdateScript : MonoBehaviour {
+	
+	void Update () {
+		if(Input.GetButtonDown("Pause")) {
+			if(Time.timeScale > 0.0f)
+			{
+				Time.timeScale = 0.0f;
+				this.gameObject.AddComponent("PauseMenuScript");
+			}
+			else
+			{
+				Time.timeScale = 1.0f;
+				Destroy(GetComponent<PauseMenuScript>());
+			}
+		}
+	}
+}
