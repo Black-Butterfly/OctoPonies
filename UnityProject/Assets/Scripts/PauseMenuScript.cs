ab990cefaa2fdef636c2ebb1c3afdbab4103c8ab
armadon13127 <jerome.rousseau@etu.univ-amu.fr>
2014-11-11 20:49:07 +0100

    Fix audio bug
    Problème lors du rechargement du niveau résolu

--- UnityProject/Assets/Scripts/PauseMenuScript.cs
+++ UnityProject/Assets/Scripts/PauseMenuScript.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class PauseMenuScript : MonoBehaviour {
 
+	private MusicScript ms;
 	private MenuTextScript mts;
 	private string[] menu;
 	private int choice = 0;
 
 	void Start()
 	{
+		ms = GameObject.Find("Music").GetComponent<MusicScript>();
+
 		Vector3 localPosition = GameObject.Find("Main Camera").transform.position;
 
 		GameObject sprite = this.transform.GetChild(0).gameObject;
@@ -52,9 +55,13 @@ public class PauseMenuScript : MonoBehaviour {
 				Destroy(this);
 				break;
 			case 1:
+				ms.StopPlaying();
+				AudioListener.pause = false;
 				Application.LoadLevel("Lvl1");
 				break;
 			case 2 :
+				ms.StopPlaying();
+				AudioListener.pause = false;
 				Application.LoadLevel("Menu");
 				break;
 			default:
